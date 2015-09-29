ffe038513d93d495525902693dc297e0397a9df4
Muccy <muccymac@gmail.com>
2015-09-29 09:57:37 +0200

    Including Pods directory for Travis CI

---
+++ Tests/Pods/Headers/Private/MUKContentFetch/MUKContentFetch.h
@@ -0,0 +1,1 @@
+../../../../../Classes/Fetch/MUKContentFetch.h
---
+++ Tests/Pods/Target Support Files/Pods/Pods-environment.h
@@ -0,0 +1,14 @@
+
+// To check if a library is compiled with CocoaPods you
+// can use the `COCOAPODS` macro definition which is
+// defined in the xcconfigs so it is available in
+// headers also when they are imported in the client
+// project.
+
+
+// MUKContentFetch
+#define COCOAPODS_POD_AVAILABLE_MUKContentFetch
+#define COCOAPODS_VERSION_MAJOR_MUKContentFetch 1
+#define COCOAPODS_VERSION_MINOR_MUKContentFetch 1
+#define COCOAPODS_VERSION_PATCH_MUKContentFetch 0
+
